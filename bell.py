e0218707d62a371cbe00c7095d73f90c307691b3
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-05 13:55:53 +0900

    jk

--- bell.py
+++ bell.py
@@ -1,3 +1,6 @@
+from xml.etree.ElementTree import ProcessingInstruction
+
+
 print ("Hello world")
 print("잘생긴")
 print("유재")
@@ -8,3 +11,5 @@ print("환호 세팅 완료")
 # 일단 변경사항 (change)에서 + 버튼 누르고
 # 소스제어 옆에 V 표시 눌러서 commit 하고
 # 변경 내용 동기화, synic 누르면 올라가는거야
+
+ProcessingInstruction
