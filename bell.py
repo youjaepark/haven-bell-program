8f0c035371a4b63ad487013244c96239a941de10
youjaepark <102572446+youjaepark@users.noreply.github.com>
2022-04-07 13:44:29 +0900

    .

--- bell.py
+++ bell.py
@@ -20,12 +20,4 @@ window.mainloop()
 
 
 
-#------------- 이줄 위에는 유재 -------------
-<<<<<<< HEAD
-print("아무거나")
-=======
->>>>>>> Stashed changes
-=======
-print("hello world")
-
->>>>>>> 3feadaf24a10e1a43a21dd1dc8634ef99dcd2362
+#------------- 이줄 위에는 유재 ------------
