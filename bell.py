3feadaf24a10e1a43a21dd1dc8634ef99dcd2362
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-07 13:40:37 +0900

    화면 크기

--- bell.py
+++ bell.py
@@ -4,8 +4,8 @@ from tkinter import *
 
 window = Tk()
 
-window.title("Gomming's Automation Tool")
-
+window.title("School Bell")
+window.geometry('800x600')
 window.mainloop()
 
 #----------- 이줄 위에는 환호 --------------
