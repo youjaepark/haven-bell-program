47a624f68c2e45117bd94085010f541e73026e7e
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-07 21:58:49 +0900

    아이콘 조절

--- bell.py
+++ bell.py
@@ -8,10 +8,10 @@ window.geometry('800x600')
 
 
 btn1 = Button(window, text="평일 벨소리", width=20, height=15)
-btn1.pack(side="left", padx=100)
+btn1.pack(side="left", padx=75)
 
 btn2 = Button(window, text="시험 벨소리", width=20, height=15)
-btn2.pack(side="right", padx=100)
+btn2.pack(side="right", padx=75)
 
 
 window.mainloop()
