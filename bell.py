7c4fd594814c8adac60666cd373795c5621d8d82
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-07 14:00:27 +0900

    환호 끝

--- bell.py
+++ bell.py
@@ -1,12 +1,20 @@
 
 
+from cgitb import text
 from tkinter import *
 window = Tk()
 window.title("School Bell")
 window.geometry('800x600')
-window.mainloop()
 
-print ("환호가")
+
+btn1 = Button(window, text="평일 벨소리", width=20, height=15)
+btn1.pack(side="left", padx=100)
+
+btn2 = Button(window, text="시험 벨소리", width=20, height=15)
+btn2.pack(side="right", padx=100)
+
+
+window.mainloop()
 
 #----------- 이줄 위에는 환호 --------------
 
