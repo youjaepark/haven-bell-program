2b83ac536176721ee2a62295ec01b136d669b8cb
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-12 13:50:34 +0900

    infront

--- bell.py
+++ bell.py
@@ -6,15 +6,28 @@ window = Tk()
 window.title("School Bell")
 window.geometry('800x600')
 
+point = 1
 
-btn1 = Button(window, text="평일 벨소리", width=20, height=15)
-btn1.pack(side="left", padx=75)
+def first(): 
+    point = 1
+    print(point)
+    Label1 = Label(window, background='green')
+    Label1.pack()
 
-btn2 = Button(window, text="시험 벨소리", width=20, height=15)
-btn2.pack(side="right", padx=75)
+def second():
+    point = 2
+    print(point)
+    btn1['bg'] = 'white'
+    btn2['bg'] = 'green'
 
+btn1 = Button(window, text="평일 벨소리", width=30, height=15, command=first)
+btn1.pack(side="left", padx=50)
+
+btn2 = Button(window, text="시험 벨소리", width=30, height=15, command= second)
+btn2.pack(side="right", padx=50)
 
 window.mainloop()
+
 
 #----------- 이줄 위에는 환호 --------------
 
