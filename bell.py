9e2e1324a64c34064a7867d886e1cf37de5c2a8e
hwanhojoung <hwanhodevelop@gmail.com>
2022-04-12 22:36:53 +0900

    시간 UI (주환아 내가 너가 import 한거 위로 올려놨어)

--- bell.py
+++ bell.py
@@ -1,7 +1,15 @@
 
 
 from cgitb import text
+from operator import truediv
+from re import A
 from tkinter import *
+import tkinter.font
+
+import schedule
+import time
+import datetime
+
 #from winsound import PlaySound
 window = Tk()
 window.title("School Bell")
@@ -9,31 +17,51 @@ window.geometry('800x600')
 
 point = "normal"
 
+font=tkinter.font.Font(family="맑은 고딕", size=25, slant="italic")
+labeltext = Label(window, text = "평일 벨소리", font=font)
+labeltext.pack(side="top", pady=30)
+
+labeltexttime = Label(window, text = datetime.datetime.now(), font=font)
+labeltexttime.pack(side="bottom", pady=30)
+
+
 def first(): 
     point = "normal"
     print(point)
-    Label1 = Label(window, background='green')
-    Label1.pack()
+    labeltext.config(text="평일 벨소리")
+    labeltext.pack()
 
 def second():
     point = "exam"
     print(point)
-    btn1['bg'] = 'white'
-    btn2['bg'] = 'green'
+    labeltext.config(text="시험 벨소리")
+    labeltext.pack()
+
+
+
+
+if (True):
+  labeltexttime.config(text=datetime.datetime.now())
+  labeltexttime.pack()
+
+
+
+
+
 
 btn1 = Button(window, text="평일 벨소리", width=30, height=15, command=first)
 btn1.pack(side="left", padx=50)
 
-btn2 = Button(window, text="시험 벨소리", width=30, height=15, command= second)
+btn2 = Button(window, text="시험 벨소리", width=30, height=15, command=second)
 btn2.pack(side="right", padx=50)
+
 
 window.mainloop()
 
 
 #----------- 이줄 위에는 환호 --------------
 
-import schedule
-import time
+
  
 if point():
  schedule.run_pending
