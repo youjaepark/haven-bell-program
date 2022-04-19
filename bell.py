c5e2f8dacda374468831132b9f5f5c9cdd4e2f4e
youjaepark <102572446+youjaepark@users.noreply.github.com>
2022-04-19 13:09:02 +0900

    jj

--- bell.py
+++ bell.py
@@ -177,7 +177,6 @@ while True:
 
 #------------- 이줄 위에는 주환 -------------
 
-#import playsound
-#playsound.playsound('sample.mp3')
-
+from playsound import playsound 
+playsound(r'C:\Users\qkrdb\OneDrive\바탕 화면\music.mp3')
 #------------- 이줄 위에는 유재 ------------
